858e6e0fd222828972c0080f603b3e3a7aea0611
AntonKhmyrov <mksenia832@gmail.com>
2023-02-25 10:01:33 +0200

    test

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+$(".firstcreen-slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoPlay: true,
+    autoplaySpeed: 2000,
+});
