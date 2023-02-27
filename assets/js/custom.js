ca543971f1b5f32ec32590be8fa32ccd1a1532ff
AntonKhmyrov <mksenia832@gmail.com>
2023-02-27 17:19:33 +0200

    test services slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,4 +18,28 @@
             },
         ]
     });
+
+    $('.services-slider').slick({
+        dots: false,
+        arrows: true,
+        appendArrows: '.services-slider-nav',
+        adaptiveHeight: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 500,
+        cssEase: 'linear',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+
+                }
+            },
+        ]
+    })
  });
