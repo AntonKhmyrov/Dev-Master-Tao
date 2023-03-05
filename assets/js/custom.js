3ce5a4b804a6665837a5a160ac4e35d18cdfcb43
AntonKhmyrov <mksenia832@gmail.com>
2023-03-05 12:43:44 +0200

    fix head-block and add form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,6 @@
     $('.banner').slick({
         dots: true,
         arrows: true,
-        appendArrows: '.banner-nav',
         adaptiveHeight: true,
         infinite: true,
         slidesToShow: 1,
@@ -19,10 +18,43 @@
         ]
     });
 
+    $('.slider-cargo-options').slick({
+        dots: false,
+        arrows: true,
+        adaptiveHeight: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 500,
+        cssEase: 'linear',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    dots: false,
+                    arrows: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+
+                }
+            },
+        ]
+
+    })
+
     $('.services-slider').slick({
         dots: false,
         arrows: true,
-        appendArrows: '.services-slider-nav',
         adaptiveHeight: true,
         infinite: true,
         slidesToShow: 3,
