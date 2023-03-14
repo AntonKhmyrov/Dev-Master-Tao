165aa7ac4bc26920e4f1b27ee5fa691b7a29c2c6
AntonKhmyrov <mksenia832@gmail.com>
2023-03-14 16:33:49 +0200

    add animation title and add new blcok advantages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -106,3 +106,34 @@
  });
 
  // ------------------------------
+
+ // transfornation mobile-slider
+
+ mobileOnlySlider("#advantages-mobile-slider", true, false, 991);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+  var slider = $($slidername);
+  var settings = {
+    mobileFirst: true,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: $breakpoint,
+        settings: "unslick"
+      }
+    ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+    if ($(window).width() > $breakpoint) {
+      return;
+    }
+    if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+    }
+  });
+
+} // Mobile Only Slider
