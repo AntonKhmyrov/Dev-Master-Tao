7ffa869e4c9bed9b460079c18c2e2a3bbb8014fb
AntonKhmyrov <mksenia832@gmail.com>
2023-04-27 17:41:35 +0300

    accrod and form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -137,3 +137,31 @@ function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
   });
 
 } // Mobile Only Slider
+
+// accordion FAQ
+
+
+const accordions = document.querySelectorAll('.tabs__accordion');
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener('click', () => {
+    const panel = accordion.nextElementSibling;
+    const tabs = accordion.parentElement;
+
+    accordion.classList.toggle('active');
+    accordion.classList.remove('active-before'); // удаляем класс при клике
+
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none';
+      tabs.style.backgroundColor = 'transparent';
+      accordion.classList.add('active-before'); // добавляем класс при скрытии
+      accordion.classList.remove('active-after');
+    } else {
+      panel.style.display = 'block';
+      tabs.style.backgroundColor = '#fff';
+      accordion.classList.add('active-after');
+      accordion.classList.remove('active-before'); // удаляем класс при показе
+    }
+  });
+});
+
